5ed8ce4652e2c5937c8be5a81c944fe1415b903c
ReygaFitra <regafitra12@gmail.com>
2022-11-22 07:31:21 +0700

    add project

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -2,6 +2,9 @@ import React from 'react';
 import heroMobile from '../Assets/images/image-hero-mobile.png';
 import heroDesktop from '../Assets/images/image-hero-desktop.png';
 import TextContents from './TextContents';
+import SvgAudioPhile from './SvgAudioPhile';
+import SvgMeet from './SvgMeet';
+import SvgMaker from './SvgMaker';
 
 const Hero = () => {
   const textContents = [{ title: 'Make Remote Work', content: 'Get your team in sync. No matter your location. streamline the processes. Create team rituals. and Watch productivity soar' }];
@@ -11,14 +14,23 @@ const Hero = () => {
       <div className="md:hidden">
         <img src={heroMobile} />
       </div>
-      <div className="hidden md:flex">
-        <img src={heroDesktop} />
-      </div>
-      <div className="w-full h-auto text-center py-10 px-1">
-        {textContents.map((textContent) => (
-          <TextContents title={textContent.title} content={textContent.content} />
-        ))}
-        <button className="px-4 py-2 bg-black text-white rounded-xl my-6">Learn More</button>
+
+      <div className="text-center md:text-start md:py-0 md:flex md:w-[100%] md:h-[100vh] md:px-5  ">
+        <div className="md:w-[100%] md:py-32 ">
+          {textContents.map((textContent) => (
+            <TextContents title={textContent.title} content={textContent.content} />
+          ))}
+          <button className="px-4 py-2 bg-black text-white rounded-xl my-4 md:mx-10 md:px-6 md:py-3">Learn More</button>
+          <div className="flex justify-evenly gap-4  md:p-5 md:mt-10">
+            <SvgAudioPhile />
+            <SvgAudioPhile />
+            <SvgMeet />
+            <SvgMaker />
+          </div>
+        </div>
+        <div className="hidden md:flex p-16 w-[100%] h-[100%] justify-center">
+          <img src={heroDesktop} className="object-cover" />
+        </div>
       </div>
     </div>
   );
--- src/Components/TextContents.js
+++ src/Components/TextContents.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const TextContents = ({ title, content }) => {
   return (
-    <div>
-      <h1 className="text-4xl font-bold">{title}</h1>
-      <p className="text-xl font-semibold my-6">{content}</p>
+    <div className="px-10 py-2 md:text-start">
+      <h1 className="text-3xl font-bold md:text-7xl ">{title}</h1>
+      <p className="text-lg font-semibold my-5 md:text-xl">{content}</p>
     </div>
   );
 };
